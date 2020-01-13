1095eabe13af50f8f3029a2aa4590212529d17b5
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-13 22:15:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,8 @@ def gross_for_director(director_data)
   index = 0 
  
   while index < director_data[:movies].length do
-    total.push += director_data[:movies][index][:worldwide_gross]
+    total += director_data[:movies][index][:worldwide_gross]
+    total.push(director_data[:movies][index][:worldwide_gross])
     
     index += 1 
   end
